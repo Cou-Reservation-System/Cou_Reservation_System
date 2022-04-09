ed4ec18aee43fa888979f032e5a05d4686eb1529
minkimhere <mj.kim.tech@gmail.com>
2022-04-10 02:58:20 +0900

    Feat: findPassword, resetPassword

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { join, login, auth } = require("../controller/admin");
+const { join, login, auth, findPassword, resetPassword } = require("../controller/admin");
 const { authMiddleware } = require("../middleware/authMiddleware")
 
 router.post("/join", join);
 router.post("/login", login)
 router.get("/auth", authMiddleware, auth)
+router.post("/findPassword", findPassword)
+router.put("/resetPassword", resetPassword)
 
 module.exports = router;
