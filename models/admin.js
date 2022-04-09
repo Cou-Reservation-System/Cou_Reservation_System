34a9da2841a64a08a30fa61254184938f38464e0
minkimhere <mj.kim.tech@gmail.com>
2022-04-09 20:45:52 +0900

    Feat: add admin

--- models/admin.js
+++ models/admin.js
@@ -1,38 +1,45 @@
-'use strict';
-const Sequelize = require('sequelize');
-const { Model } = require('sequelize');
+"use strict";
+const Sequelize = require("sequelize");
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    class Admin extends Model {
-        static associate(models) {
-        
-        }
+  class Admin extends Model {
+    static associate(models) {}
+  }
+  Admin.init(
+    {
+      adminId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+
+      id: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+      },
+
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+      },
+
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+      },
+
+      salt: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+      },
+    },
+    {
+      sequelize,
+      timestamps: true,
+      modelName: "Admin",
     }
-    Admin.init(
-        {
-            adminId: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER,
-            },
-
-            id: {
-                allowNull: false,
-                type: Sequelize.STRING(30),
-            },
-
-            password: {
-                allowNull: false,
-                type: Sequelize.STRING(30),
-            },
-
-        },
-        {
-            sequelize,
-            timestamps: true,
-            modelName: 'Admin',
-        },
-    );
-    return Admin;
+  );
+  return Admin;
 };
