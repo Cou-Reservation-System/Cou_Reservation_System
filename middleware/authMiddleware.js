432ffdf390061bb98984d61e4daacfca22e8ac3f
minkimhere <mj.kim.tech@gmail.com>
2022-04-09 23:01:41 +0900

    Feat: add authMiddleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const { Admin } = require('../models');
+
+module.exports.authMiddleware = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const [tokenType, tokenValue] = authorization.split(' ');
+
+    if (tokenType !== 'Bearer') {
+      return res.json({
+        ok: false,
+        errorMessage: '로그인 후 이용 가능한 기능입니다.',
+      });
+    }
+
+    const { adminId } = jwt.verify(tokenValue, process.env.TOKENKEY);
+    const admin = await Admin.findOne({ where: { adminId } });
+    res.locals.admin = admin;
+
+    next();
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return res.json({
+        ok: false,
+        errorMessage: '로그인 유효기간이 지났습니다. 다시 로그인 해주세요.',
+      });
+    }
+
+    console.error(`${err}에러로 로그인 확인을 실패하였습니다.`);
+
+    res.status(400).json({
+      ok: false,
+      errorMessage: '로그인 확인에 실패하였습니다.',
+    });
+  }
+};
