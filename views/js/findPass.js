6f20156c93ad3b657db462114a9406da8591aa71
HyunjiKim818 <kjhxxxx@gmail.com>
2022-04-10 05:21:24 +0900

    Edited findPass.css 0410.1

--- views/js/findPass.js
+++ views/js/findPass.js
@@ -1,3 +1,22 @@
+//초기 세팅
+function findPassSetting() {
+    const temp_html = `
+        <div class="inputWrap" id="inputWrap">
+         <div class="id">
+            <input id="id" type="text" placeholder="아이디" />
+             </div>
+             <div class="email">
+             <input id="email" type="text" placeholder="EMAIL" />
+             </div>
+             </div>
+             <div class="btnWrap">
+             <button onclick="findPass()" id="findPassBtn" type="button">입력하기</button>
+             </div>
+             `
+    $('#mainWrap').append(temp_html);
+
+}
+
 //비밀번호 찾기 페이지 이동
 function findPass() {
     $.ajax({
@@ -8,26 +27,39 @@ function findPass() {
             email: $("#email").val()
         },
         success: function (response) {
-            
+            const id = response.id
+            $('#id').hide()
+            $('#email').hide()
+            $('#findPassBtn').hide()
+            const temp_html = `<div class="inputWrap" id="inputWrap">
+            <div class="newPass">
+               <input id="newPass" type="text" placeholder="새 비밀번호" />
+                </div>
+                <div class="newPassCon">
+                <input id="newPassCon" type="text" placeholder="새 비밀번호 확인" />
+                </div>
+                </div>
+                <div class="btnWrap">
+                <button onclick="changePass(${id})" id="changePassBtn" type="button">비밀번호 변경하기</button>
+                </div>`
+            $('#mainWrap').append(temp_html);
+   
         }
     });
  };
 
-function findPassBtn () {
+function changePassBtn (id) {
     $.ajax({
         type: "PUT",
         url: "/admin/resetPassword",
         data: {
-            id: $("#id").val(),
-            email: $("#email").val()
+            id,
+            newPass: $("#newPass").val(),
+            newPassCon: $("#newPassCon").val()
         },
         success: function (response) {
-            
+            window.location.replace("/")
 
         }
     });
 };
-
-
-
-
