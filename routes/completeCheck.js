9aade18f598711709a3bfea69d2e76f910a7e5f7
Tacocat3 <yongjusung3351@gmail.com>
2022-04-11 05:56:47 +0900

    authMiddleWare설정

--- routes/completeCheck.js
+++ routes/completeCheck.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { receivingCheck } = require("../controller/completeCheck")
+const { authMiddleware } = require("../middleware/authMiddleware")
 
-router.put("/check/receiving", receivingCheck)
+router.put("/check/receiving",authMiddleware, receivingCheck)
 
 module.exports = router;
