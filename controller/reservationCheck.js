58f6ff9e6fbac727dd816c25942dc06e54cb3a0a
Tacocat3 <yongjusung3351@gmail.com>
2022-04-10 20:46:59 +0900

    싱글 테이블 완료/미완료 구현, 전체조회시 미완료된 예약만 불러오도록 수정

--- controller/reservationCheck.js
+++ controller/reservationCheck.js
@@ -1,7 +1,7 @@
-const { Coupang, sequelize } = require('../models');
+const { sequelize } = require('../models');
 const { QueryTypes } = require('sequelize');
 
-//예약 전체 조회하기
+//예약 전체 조회하기 (미완료만)
 module.exports.checkEveryReservation = async (req, res) => {
   await checkEveryReservation(req, res);
 };
@@ -13,9 +13,12 @@ module.exports.checkDailyReservation = async (req, res) => {
 
 async function checkEveryReservation(req, res) {
   try {
-    const reservedList = await sequelize.query('select * from coupangs union all select * from 3pl', {
+    const reservedList = await sequelize.query(
+      'select * from coupangs where isDone = 0 union all select * from 3pl where isDone =0',
+      {
         type: QueryTypes.SELECT,
-      });
+      }
+    );
     res.status(200).json({
       reservedList,
       ok: true,
@@ -31,14 +34,28 @@ async function checkEveryReservation(req, res) {
 
 async function checkDailyReservation(req, res) {
   try {
-    const date = req.query.date
-    const reservedList = await sequelize.query(`select * from coupangs where \'dateAndTime\' = ${date} union all select * from 3pl where \'dateAndTime\' = ${date}` , {
-    })
+    const date = req.query.DateAndTime;
+    const reservedList = await sequelize.query(
+      `select * from coupangs where DateAndTime = \'${date}\' union all select * from 3pl where DateAndTime = \'${date}\'`,
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
+    const countReservedList = await sequelize.query(
+      `select sum(CNT) as totalCount from (select count(*) as CNT from coupangs where DateAndTime = \'${date}\'
+    union all
+    select count(*) as CNT from 3pl where DateAndTime = \'${date}\') ad`,
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
+    const totalReservedList = countReservedList[0].totalCount;
     res.status(200).json({
-        reservedList,
-        ok: true,
-        message: '조회가 완료되었습니다.',
-    })
+      reservedList,
+      totalReservedList,
+      ok: true,
+      message: '조회가 완료되었습니다.',
+    });
   } catch (error) {
     res.status(400).json({
       ok: false,
